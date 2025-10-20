dacbf847ca79b00020f655426be49d72062be20a
casoch <casoch@gmail.com>
2025-10-20 11:03:01 +0200

    Clase 20 oct

--- src/ejemplos_arrays/Principal.java
+++ src/ejemplos_arrays/Principal.java
@@ -38,7 +38,7 @@ public class Principal {
 
     public static void ejercicio53()
     {
-        final int MAX = 20;
+        final int MAX = 4;
         int[] numeros = new int[MAX];
         for (int i = 0; i < MAX; i++) {
             numeros[i] = i;
@@ -74,10 +74,10 @@ public class Principal {
         //              números random del 0 al 10
         final int MAX = 10;
         int[] numeros = new int[MAX];
+        int min = 0;
+        int max = 10;
 
         for (int i = 0; i < MAX; i++) {
-            int min = 0;
-            int max = 10;
             int numeroRandom = (int)(Math.random() * ((max - min) + 1)) + min;
             numeros[i] = numeroRandom;
         }
@@ -87,14 +87,117 @@ public class Principal {
             suma += numeros[i];
         }
         System.out.println("Suma: "+suma);
+
 //        -	La media aritmética de los 10 números
+        double media = (suma*1.0)/MAX;
+        System.out.println("Media: "+media);
+
 //        -	Los números que son superiores a la media
+        System.out.println("Números por encima de la media");
+        for (int i = 0; i < MAX; i++) {
+            if (numeros[i] > media)
+                System.out.println(numeros[i]);
+        }
 //        -	El número máximo
+        System.out.println("Contenido del array");
+        for (int i = 0; i < MAX; i++) {
+            System.out.println("["+i+"]: "+numeros[i]);
+        }
+        int maximo = numeros[0];
+        for (int i = 1; i < MAX; i++) {
+           if (numeros[i] > maximo) {
+                maximo = numeros[i];
+            }
+        }
+        System.out.println("El más grande es: " + maximo);
+
 //        -	El número mínimo
+        int minimo = numeros[0];
+        for (int i = 1; i < MAX; i++) {
+            if (numeros[i] < minimo) {
+                minimo = numeros[i];
+            }
+        }
+        System.out.println("El más pequeño es: " + minimo);
 
     }
 
+    public static void ejercicio58()
+    {
+        final int MAX = 10;
+        int[] numeros = new int[MAX];
+        int min = 0;
+        int max = 10;
+
+        for (int i = 0; i < MAX; i++) {
+            int numeroRandom = (int)(Math.random() * ((max - min) + 1)) + min;
+            numeros[i] = numeroRandom;
+        }
+
+        int[] numerosReverse = new int[numeros.length];
+
+        //Primero, intentamos hacer una copia exacta
+        for (int i = 0; i < MAX; i++) {
+            numerosReverse[i] = numeros[i];
+        }
+
+        //Ahora, hacemos la copia inversa
+        for (int i = 0, j=MAX-1; i < MAX; i++, j--) {
+            numerosReverse[j] = numeros[i];
+        }
+
+        //¿Y sólo con la i se podría hacer?
+        for (int i = 0; i < MAX; i++) {
+            numerosReverse[MAX-i-1] = numeros[i];
+        }
+
+        for (int i = 0; i < MAX; i++) {
+            System.out.println("numeros["+i+"]="+numeros[i]);
+            System.out.println("numerosReverse["+(MAX-i-1)+"]="+numerosReverse[MAX-i-1]);
+        }
+    }
+
+    public static void ejercicio59()
+    {
+        final int MAX = 10;
+        int[] numeros = new int[MAX];
+        int min = 0, max = MAX;
+
+        for (int i = 0; i < MAX; i++) {
+            numeros[i] = (int)(Math.random() * ((max - min) + 1)) + min;;
+        }
+
+        //Declarar los dos arrays "mini"
+        int[] numsPares = new int[MAX/2];
+        int[] numsImpares = new int[MAX/2];
+
+        //Rellenarlos
+        for (int i = 0; i < MAX; i++) {
+            if (i%2 == 0) //Casilla par
+            {
+                numsPares[i/2] = numeros[i];
+            }
+            else //Casilla impar
+            {
+                numsImpares[i/2] = numeros[i];
+            }
+        }
+    }
+
+    public static void ejercicio62()
+    {
+        //PASO 1: Pedir al usuario el número de pizzas
+
+        //PASO 2: Declarar y pedir memoria para un array
+        //          que pueda guardar ese número de pizzas (los nombres)
+
+        //PASO 3: Mostrar el menú hasta que el usuario escoja la opción de salir
+        //          (bucle)
+
+        //PASO 4: Implementar cada una de las opciones del menú (dentro del bucle del paso 3)
+    }
+
     public static void main(String[] args) {
-        ejercicio56();
+        ejercicio58();
     }
 }
